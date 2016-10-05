890ae942879397026bce5b8e135b855cc31643e0
jonathanViralbamboo <jonathan@viralbamboo.com>
2016-10-05 11:51:48 +0100

    Spriting and Production Stylesheets
    Before we upload our Compass project files to a server, we'll need to
    set new configurations for our production environment and optimize the
    CSS output.

--- Spritesheets/config.rb
+++ Spritesheets/config.rb
@@ -7,19 +7,7 @@ sass_dir = "sass"
 images_dir = "img"
 fonts_dir = "fonts"
 javascripts_dir = "js"
-
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
+# http_images_path = "http://my-site.com/img"
+# http_generated_images_path = "http://assets.my-prj.com/img"
+# environment = :production
 relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
